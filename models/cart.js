7f8828830f6e524db26ca7586d84a88e2327ad9e
HimanshuSinghRaghav <hsinghraghav1@gmail.com>
2023-08-27 14:58:17 +0530

    add:-"cart routes"

--- models/cart.js
+++ models/cart.js
@@ -13,7 +13,8 @@ const cartSchema = new mongoose.Schema({
             },
             quantity:Number
         }
-    ]
+    ],
+    order:String
 },{timestamps:true})
 
 export default mongoose.model('Cart' , cartSchema)
